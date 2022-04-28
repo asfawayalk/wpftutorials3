8d7f811380d22fd81c1643bb65f1e9384537e714
asfaw <asfaw.aylkibet@gmail.com>
2022-04-28 12:58:48 -0700

    initial

---
+++ ClassCommands/Models/OwnedItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassCommands.Models
+{
+    public class OwnedItem
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+    }
+}
+
+
+
+//comment
